8ad131c55d61be13fa5ecb58ab988e5ff2a07074
Kamran <Home@MacBook-Pro-5.local>
2020-05-28 13:53:07 -0600

    =new

--- ddd.py
+++ ddd.py
@@ -2,8 +2,9 @@ import os
 import sys
 
 ffile=sys.argv[1]
-for i in xrange(8):
-#    os.system('cp runMogul%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
+nn=int(sys.argv[2])
+for i in xrange(nn):
+    os.system('cp runMogul%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 #    os.system('cp runFillmore%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
-    os.system('cp runFillmore/Run%s/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
+#    os.system('cp runFillmore/Run%s/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 
