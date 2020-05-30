897396cf644a0d60041a43ae2dc25ffa8bd58899
kamran.karimi1 <kkarimi62>
2020-05-29 21:04:32 -0600

    =add

---
+++ runOklahoma/Run1/oarScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run1
+
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run1/swarmOklahoma.ipynb ./output.ipynb -p Df 2.0 -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Oklahoma/TableS1.csv'
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run1/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm ./output.html
---
+++ runOklahoma/Run12/oarScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run12
+
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run12/swarmOklahoma.ipynb ./output.ipynb -p Df 2.0 -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Oklahoma/TableS1.csv'
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run12/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm ./output.html
