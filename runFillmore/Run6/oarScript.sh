5bed8fa7ff0e5946a260d547b6d5e3a4488be90e
kamran.karimi1 <kkarimi62>
2020-06-03 12:18:11 -0600

    =add

--- runFillmore/Run6/oarScript.sh
+++ runFillmore/Run6/oarScript.sh
@@ -2,5 +2,5 @@
 
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runFillmore/Run6
 
-papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runFillmore/Run6/Fillmore2015.ipynb ./output.ipynb -p Df 2.5 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt'
-jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runFillmore/Run6/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runFillmore/Run6/Fillmore2015.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt'
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runFillmore/Run6/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm ./output.html
