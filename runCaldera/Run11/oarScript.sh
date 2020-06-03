4cdff3cb393e5ef4c34d1a78bf3bdcacce6f5b21
kamran.karimi1 <kkarimi62>
2020-06-03 09:39:58 -0600

    =add

---
+++ runCaldera/Run11/oarScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runCaldera/Run11
+
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runCaldera/Run11/swarmCaldera.ipynb ./output.ipynb -p Df 2.0 -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/LongValleyCaldera/catalog1st.csv'
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runCaldera/Run11/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm ./output.html
