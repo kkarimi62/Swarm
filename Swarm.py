69970fea8d02b6fccf06b9042c1a20f48b61cc43
kamran.karimi1 <kkarimi62>
2020-06-04 16:48:05 -0600

    =add

--- Swarm.py
+++ Swarm.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = 16
-	jobname  = 'runFillmore' #'runFillmore2015' 
+	jobname  = 'runMogul' #'runFillmore2015' 
 	readPath = os.getcwd() # + '/CLUSTER' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
@@ -20,8 +20,8 @@ if __name__ == '__main__':
 	argv = "-p quantile 0.05"
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Oklahoma/TableS1.csv') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/LongValleyCaldera/catalog1st.csv') 
-#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/hypodd.reloc.mag') 
-	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt') 
+	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/hypodd.reloc.mag') 
+#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/YuhaDesert/EMC.csv') 
 	PYFILdic = { 
 		0:'DifferentThresholds.ipynb',
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 		4:'swarmCaldera.ipynb',
 		5:'swarmYuhaDesert.ipynb'
 		}
-	keyno = 2
+	keyno = 1
 #---
 #---
 	PYFIL = PYFILdic[ keyno ] 
