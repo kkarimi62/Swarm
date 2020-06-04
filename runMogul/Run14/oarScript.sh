1e870005ae4adb0bcd567a9321d209c88cc83067
kamran.karimi1 <kkarimi62>
2020-06-04 02:34:26 -0600

    =add

--- runMogul/Run14/oarScript.sh
+++ runMogul/Run14/oarScript.sh
@@ -3,4 +3,4 @@
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runMogul/Run14
 
 papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runMogul/Run14/Mogul2008.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Mogul2008/hypodd.reloc.mag'
-jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runMogul/Run14/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runMogul/Run14/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm output.html
--- runMogul/Run8/oarScript.sh
+++ runMogul/Run8/oarScript.sh
@@ -3,4 +3,4 @@
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runMogul/Run8
 
 papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runMogul/Run8/Mogul2008.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Mogul2008/hypodd.reloc.mag'
-jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runMogul/Run8/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runMogul/Run8/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm output.html
