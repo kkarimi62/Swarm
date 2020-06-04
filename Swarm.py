59e667abb2ce0b647a7f726e2829376a2c002e16
kamran.karimi1 <kkarimi62>
2020-06-04 00:49:15 -0600

    =add

--- Swarm.py
+++ Swarm.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = 16
-	jobname  = 'runCaldera' #'runMogul' #'runFillmore2015' 
+	jobname  = 'runMogul' #'runMogul' #'runFillmore2015' 
 	readPath = os.getcwd() # + '/CLUSTER' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
@@ -19,8 +19,8 @@ if __name__ == '__main__':
 #	argv = "-p Df 2.0 -p quantile 0.05"
 	argv = "-p quantile 0.05"
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Oklahoma/TableS1.csv') 
-	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/LongValleyCaldera/catalog1st.csv') 
-#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/catsearch.14436') 
+#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/LongValleyCaldera/catalog1st.csv') 
+	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/catsearch.14436') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/YuhaDesert/EMC.csv') 
 	PYFILdic = { 
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 		4:'swarmCaldera.ipynb',
 		5:'swarmYuhaDesert.ipynb'
 		}
-	keyno = 4
+	keyno = 1
 #---
 #---
 	PYFIL = PYFILdic[ keyno ] 
---
+++ runMogul/Run5/oarScript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runMogul/Run5
+
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runMogul/Run5/Mogul2008.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Mogul2008/catsearch.14436'
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runMogul/Run5/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm output.html
