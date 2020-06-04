1ce1d9cad68fa092db76408cb21a46781a1aab22
kamran.karimi1 <kkarimi62>
2020-06-04 15:42:07 -0600

    =add

--- Swarm.py
+++ Swarm.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = 16
-	jobname  = 'runMogul' #'runFillmore2015' 
+	jobname  = 'runOklahoma' #'runFillmore2015' 
 	readPath = os.getcwd() # + '/CLUSTER' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
@@ -18,9 +18,9 @@ if __name__ == '__main__':
 	partition = 'single' #'bigmem' #'single' #'parallel' #'single'
 #	argv = "-p Df 2.0 -p quantile 0.05"
 	argv = "-p quantile 0.05"
-#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Oklahoma/TableS1.csv') 
+	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Oklahoma/TableS1.csv') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/LongValleyCaldera/catalog1st.csv') 
-	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/hypodd.reloc.mag') 
+#	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/hypodd.reloc.mag') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/YuhaDesert/EMC.csv') 
 	PYFILdic = { 
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 		4:'swarmCaldera.ipynb',
 		5:'swarmYuhaDesert.ipynb'
 		}
-	keyno = 1
+	keyno = 3
 #---
 #---
 	PYFIL = PYFILdic[ keyno ] 
--- runOklahoma/Run11/oarScript.sh
+++ runOklahoma/Run11/oarScript.sh
@@ -3,4 +3,4 @@
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run11
 
 papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run11/swarmOklahoma.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/Oklahoma/TableS1.csv'
-jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run11/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runOklahoma/Run11/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;rm output.html
