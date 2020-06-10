2c82bbb814ec4d66ed9f4d4d2e21b650df32cdbe
kamran.karimi1 <kkarimi62>
2020-06-10 11:40:28 -0600

    =add

--- runYuha/Run0/oarScript.sh
+++ runYuha/Run0/oarScript.sh
@@ -2,6 +2,5 @@
 
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/runYuha/Run0
 
-rm output.html;rm corrDim.png
 papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/runYuha/Run0/swarmYuhaDesert.ipynb ./output.ipynb -p quantile 0.05 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/YuhaDesert/EMC.csv'
-jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runYuha/Run0/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True --debug;ls output.html
+jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/runYuha/Run0/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html
