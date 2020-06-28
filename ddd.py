840d082f0d15bb60fdfc0e4a8c03cb27d48e5be6
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2020-06-28 00:28:54 -0600

    =add

--- ddd.py
+++ ddd.py
@@ -4,8 +4,9 @@ import sys
 ffile=sys.argv[1]
 nn=int(sys.argv[2])
 for i in xrange(nn):
+    os.system('cp runRidgeCrest%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 #    os.system('cp runMogul%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 #    os.system('cp runFillmore%s/Run0/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 #    os.system('cp runFillmore/Run%s/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
-    os.system('cp runOklahoma/Run%s/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
+#    os.system('cp runOklahoma/Run%s/%s.png ../../%s.%s.png'%(i,ffile,ffile,i))
 
