caa5e3db2f298c3b0c133d379b143447fa4f140e
kamran.karimi1 <kkarimi62>
2020-10-08 13:41:21 -0600

    Yuha case

--- Swarm.py
+++ Swarm.py
@@ -10,14 +10,14 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = 1
-	jobname  = 'swarmYuhaDesertFirstPeriod1' #'yuhaDesertFit2nd' #'runRidgeCrest' #'runFillmore2015' 
+	jobname  = 'swarmYuhaDesertFirstPeriod0' #'yuhaDesertFit2nd' #'runRidgeCrest' #'runFillmore2015' 
 	readPath = os.getcwd() # + '/CLUSTER' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
 	mem = '256gb' #'4gb' #'64gb'
 	partition = 'bigmem' #'single' #'parallel' #'single'
-#	argv = "-p BVAL 0.84 -p MC 1.5"
-	argv = "-p BVAL 1.30 -p MC 3.0"
+	argv = "-p BVAL 0.84 -p MC 1.5"
+#	argv = "-p BVAL 1.30 -p MC 3.0"
 #	argv = "-p Df 2.0 -p quantile 0.05"
 #	argv = "-p quantile 0.05"
 #	argv = "-p n_thresh 0.001"
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/LongValleyCaldera/catalog1st.csv') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Mogul2008/hypodd.reloc.mag') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/Fillmore2015/SRL-2016020_esupp_Table_S2.txt') 
-	argv = " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/YuhaDesert/EMC.csv') 
+	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/YuhaDesert/EMC.csv') 
 #	argv += " -p SWARM_PATH \'%s\'"%(readPath+'/dataset/RidgeCrest/DataS1_noXYZ.txt') 
 #	argv += " -p SWARM_PATH3 \'%s\'"%(readPath+'/dataset/RidgeCrest/catalog_fmsearch.csv') 
 #	argv += " -p SWARM_PATH4 \'%s\'"%(readPath+'/dataset/RidgeCrest/momentTensors.pkl') 
--- swarmYuhaDesertFirstPeriod0/Run0/oarScript.sh
+++ swarmYuhaDesertFirstPeriod0/Run0/oarScript.sh
@@ -2,5 +2,5 @@
 
 EXEC_DIR=/home/kamran.karimi1/Project/git/Swarm/swarmYuhaDesertFirstPeriod0/Run0
 
-papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/swarmYuhaDesertFirstPeriod0/Run0/swarmYuhaDesertFirstPeriod.ipynb ./output.ipynb  -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/YuhaDesert/EMC.csv'
+papermill --prepare-only /home/kamran.karimi1/Project/git/Swarm/swarmYuhaDesertFirstPeriod0/Run0/swarmYuhaDesertFirstPeriod.ipynb ./output.ipynb -p BVAL 0.84 -p MC 1.5 -p SWARM_PATH '/home/kamran.karimi1/Project/git/Swarm/dataset/YuhaDesert/EMC.csv'
 jupyter nbconvert --execute /home/kamran.karimi1/Project/git/Swarm/swarmYuhaDesertFirstPeriod0/Run0/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html
